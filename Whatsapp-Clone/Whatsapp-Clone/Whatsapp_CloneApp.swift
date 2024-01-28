f4d63bc4660b97d8f55b4013664bc0e3f9033f02
AbhilashPalem258 <abhilashkumar258@gmail.com>
2024-01-28 23:20:30 +0530

    whatsapp clone added

---
+++ Whatsapp-Clone/Whatsapp-Clone/Whatsapp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whatsapp_CloneApp.swift
+//  Whatsapp-Clone
+//
+//  Created by Abhilash Palem on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Whatsapp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
